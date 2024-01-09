5dffe76465adcd55a091d67e4f8e351d34b619a3
Sam Hernandez <samhernandez@Sams-MacBook-Pro.local>
2024-01-09 05:02:27 -0800

    fix serializer and allow cors *

--- app/serializers/page_serializer.rb
+++ app/serializers/page_serializer.rb
@@ -1,7 +1,8 @@
 class PageSerializer < ActiveModel::Serializer
-  attributes :id, :owner_address, :collection_address, :title, :description, :template
-  
-  def include_description?
+  attributes :id, :owner_address, :collection_address, :title, :template
+  attribute :description, if: :show_description?
+
+  def show_description?
     @instance_options[:show_description]
   end
 end
